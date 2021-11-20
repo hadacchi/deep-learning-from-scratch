caa5545a19ce8afc6b5ff36c616dba5067ef94d4
hadacchi <hadacchi@users.noreply.github.com>
2021-11-20 13:56:27 +0900

    done to chapter 3.3

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 
 - ch01. コードなし
 - ch02. コードは `ch02.py` へ実装
+- ch03, コードは `ch03.py` へ実装
 
 ## 免責
 
@@ -16,8 +17,13 @@
 - python
     - mini-forge MacOSX arm64版
     - `python -VV` の出力
-    - Python 3.9.7 | packaged by conda-forge | (default, Sep 29 2021, 19:22:19)
-      [Clang 11.1.0 ]
+      ```
+      Python 3.9.7 | packaged by conda-forge | (default, Sep 29 2021, 19:22:19) [Clang 11.1.0 ]
+      ```
+    - activate は次のコマンド
+      ```shell
+      conda activate dl-env
+      ```
 
 # note
 
@@ -29,3 +35,14 @@
 - パーセプトロンの実装
 - ch02.py を参照のこと
 - NODEというパーセプトロンの基本の箱にlambda関数で返却する数式を定義できるようにした
+
+
+## ch3
+
+- ch03.py を， ch02.py を利用して実装
+- 活性化関数をプロットし可視化
+    - step関数
+    - sigmoid関数
+    - ReLU関数
+- 行列計算の動作確認
+
---
+++ ch03.py
@@ -0,0 +1,74 @@
+import numpy as np
+import timeit
+
+import matplotlib.pyplot as plt
+
+w1 = 0.5
+w2 = 0.5
+w = np.array([w1, w2])
+theta = 0.7
+
+# activation function
+def step(x):
+    if hasattr(x, '__len__'):
+        return np.array([1 if e > 0 else 0 for e in x])
+    return 1 if x > 0 else 0
+
+def sigmoid(x):
+    if hasattr(x, '__len__'):
+        return np.array([1/(1+np.exp(-e)) for e in x])
+    return 1/(1+np.exp(-x))
+
+def relu(x):
+    return np.maximum(0,x)
+
+# show function form
+def plot_function(h, title):
+    x = np.arange(-5,5,0.1)
+    plt.plot(x,h(x))
+    plt.title(title)
+    plt.savefig(f'{title}.png')
+    plt.close('all')
+
+
+# X           W         Y
+#
+# [ x1 ] ---- w11 ----> [ y1 ]
+# [ x1 ] ---- w21 ----> [ y2 ]
+# [ x1 ] ---- w31 ----> [ y3 ]
+#
+# [ x2 ] ---- w12 ----> [ y1 ]
+# [ x2 ] ---- w22 ----> [ y2 ]
+# [ x2 ] ---- w32 ----> [ y3 ]
+#
+
+x1 = 3
+x2 = 7
+
+w11 = .1
+w21 = .5
+w31 = .3
+
+w12 = .3
+w22 = .2
+w32 = .7
+
+X = np.array([ x1 , x2 ])
+
+W = np.array([[ w11, w21, w31 ], [ w12, w22, w32 ]])
+
+
+if __name__ == '__main__':
+    # output function response for debugging
+
+    print('step function')
+    plot_function(step, 'step function')
+    print('sigmoid function')
+    plot_function(sigmoid, 'sigmoid function')
+    print('ReLU function')
+    plot_function(relu, 'ReLU function')
+
+    print(f'X = \n{X}')
+    print(f'W = \n{W}')
+    print(f'Y = \n{np.dot(X,W)}')
+
