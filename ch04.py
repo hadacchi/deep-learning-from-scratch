09040be61c2e35b20bc82544f6922ffd17e06fc3
hadacchi <hadacchi@users.noreply.github.com>
2021-12-14 23:57:55 +0900

    update ch04

--- ch04.py
+++ ch04.py
@@ -1,15 +1,14 @@
 import numpy as np
 
+
 def cross_entropy_error(y, t):
-    '''t is one hot true label
+    """t is one hot true label
     y is output of neural network
     t and y have k-dimensional value, where k is the number of label
-    '''
+    """
 
-    out = 0
     EPS = 1.0e-10
-
     t = np.array(t)
     y = np.array(y)
 
-    return -(t * np.log(y)).sum()
+    return -(t * np.log(y + EPS)).sum()
